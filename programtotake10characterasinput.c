66b886ebcae84fd46952abf1150e86b1f7546d17
Mobassir <mobassirhossen94@gmail.com>
2017-02-05 13:02:41 +0600

    Add files via upload

---
+++ programtotake10characterasinput.c
@@ -0,0 +1,14 @@
+/*
+problem : write a program that will take 10 characters string as input and will print the string on the
+ console(without using loop)
+ */
+ #include<stdio.h>
+ int main(int argc,char *argv[])
+ {
+    char str[100];
+
+      scanf("%10s",str);
+    printf(str);
+
+     return 0;
+ }

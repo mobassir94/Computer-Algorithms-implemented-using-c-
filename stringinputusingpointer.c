ab809be29942ef74a1437351bbe4c68bf9471e26
Mobassir <mobassirhossen94@gmail.com>
2017-02-05 12:51:51 +0600

    Add files via upload

---
+++ stringinputusingpointer.c
@@ -0,0 +1,13 @@
+/*
+problem : write a program that will take string as input(using pointer) and will print the string on the
+ console(without using array)
+ */
+ #include<stdio.h>
+ int main()
+ {
+    char *p,*q;
+    p=gets(q);
+    printf(p);
+
+     return 0;
+ }

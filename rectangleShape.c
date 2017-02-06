1a8cc2fa5a928e902176daa2dccfc149e7c54c2c
Mobassir <mobassirhossen94@gmail.com>
2017-02-06 15:02:16 +0600

    Add files via upload

---
+++ rectangleShape.c
@@ -0,0 +1,27 @@
+/*
+ write a program to create a rectangle shape(by printing star *) in c
+
+*/
+#include<stdio.h>
+int main()
+{
+  int m,n,i;
+
+    printf("Enter the input of M:\n");
+    scanf("%d",&m);
+    printf("Enter the input of N(N should be less then M):\n");
+    scanf("%d",&n);
+      for(i=0; i<m; i++)printf("*");
+      printf("\n");
+      while(n--){
+    for(i=0; i<m; i++)
+    {
+        if(i==0 || i==m-1) {
+            printf("*");
+        } else printf(" ");
+    }
+    printf("\n");
+      }
+      for(i=0; i<m; i++) printf("*");
+    return 0;
+}
